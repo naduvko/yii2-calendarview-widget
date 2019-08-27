c5e223af3ab1a7f5cc62e2406bfc40e8492780e1
Ivan Chivchibashiev <naduvko@gmail.com>
2019-08-27 14:35:34 +0300

    month

--- views/month.php
+++ views/month.php
@@ -3,29 +3,21 @@
     <th colspan="8">
         <div class="btn-group btn-group-justified">
                 <a href="" class="navigate btn btn-primary" data-show="<?=$monthRendered-1?>" data-hide="<?=$monthRendered?>">&#8678; <?=Yii::t('calendar','Previous')?></a>
-                <a href="" class="navigate btn btn-primary disabled d-xs-none d-sm-none"><?=$title?></a>
-                <a href="" class="navigate btn btn-primary disabled d-md-none d-lg-none"><?=$title?></a>
+                <a href="" class="navigate btn btn-primary disabled"><?=$title?></a>
+                <!-- <a href="" class="navigate btn btn-primary disabled d-md-none d-lg-none"><?=$title?></a> -->
                 <a href="" class="navigate btn btn-primary" data-show="<?=$monthRendered+1?>" data-hide="<?=$monthRendered?>"><?=Yii::t('calendar','Next')?> &#8680;</a>
         </div>
     </th>
 </thead>
 <thead>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Week')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Monday')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Tuesday')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Wednesday')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Thursday')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Friday')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Saturday')?></th>
-<th class="d-xs-none d-sm-none"><?=Yii::t('calendar','Sunday')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','W')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','Mo')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','Tu')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','We')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','Th')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','Fr')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','Sa')?></th>
-<th class="d-md-none d-lg-none"><?=Yii::t('calendar','Su')?></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Week')?></div><div class="d-lg-none"><?=Yii::t('calendar','W')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Monday')?></div><div class="d-lg-none"><?=Yii::t('calendar','Mo')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Tuesday')?></div><div class="d-lg-none"><?=Yii::t('calendar','Tu')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Wednesday')?></div><div class="d-lg-none"><?=Yii::t('calendar','We')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Thursday')?></div><div class="d-lg-none"><?=Yii::t('calendar','Th')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Friday')?></div><div class="d-lg-none"><?=Yii::t('calendar','Fr')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Saturday')?></div><div class="d-lg-none"><?=Yii::t('calendar','Sa')?></div></th>
+<th><div class="d-xs-none d-sm-none d-md-inline"><?=Yii::t('calendar','Sunday')?></div><div class="d-lg-none"><?=Yii::t('calendar','Su')?></div></th>
 </thead>
 
 <?=$content?>
